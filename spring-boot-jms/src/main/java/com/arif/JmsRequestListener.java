72f12cd2f01f795a4f7940fcf47093d7ee9ca408
aashaikh1 <57527098+aashaikh1@users.noreply.github.com>
2019-12-07 20:15:22 +1100

    Add files via upload

---
+++ spring-boot-jms/src/main/java/com/arif/JmsRequestListener.java
@@ -0,0 +1,32 @@
+package com.arif;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+/**
+ *
+ * @author arif
+ */
+@Component
+public class JmsRequestListener {
+
+    @Autowired
+    private JmsMessageHandler messageHandler;
+
+    @Autowired
+    private JmsHelper jmsHelper;
+    
+    @JmsListener(destination = "${input.queue.name}")
+    @SendTo("${reply.queue.name}")
+    public Message consumeMesssage(Message message) throws JMSException {
+        String replyPayload = "Your Message has been received";
+        messageHandler.handleRequestReceivedMessage(message);
+        Message replyMessage = jmsHelper.createMessage(replyPayload, message.getJMSMessageID());//set correlation id
+        return replyMessage;
+    }
+
+}
