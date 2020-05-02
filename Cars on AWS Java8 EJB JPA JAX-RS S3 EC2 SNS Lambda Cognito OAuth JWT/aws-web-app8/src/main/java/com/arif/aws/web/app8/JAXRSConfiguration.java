972da940dae175096a7d31ecc54442e974a5d97e
aashaikh1 <arif-ashaikh@usa.net>
2020-05-03 04:35:15 +1000

    .

--- Cars on AWS Java8 EJB JPA JAX-RS S3 EC2 SNS Lambda Cognito OAuth JWT/aws-web-app8/src/main/java/com/arif/aws/web/app8/JAXRSConfiguration.java
+++ Cars on AWS Java8 EJB JPA JAX-RS S3 EC2 SNS Lambda Cognito OAuth JWT/aws-web-app8/src/main/java/com/arif/aws/web/app8/JAXRSConfiguration.java
@@ -3,7 +3,7 @@ package com.arif.aws.web.app8;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("resources")
+@ApplicationPath("resources") 
 public class JAXRSConfiguration extends Application {
     
 }
